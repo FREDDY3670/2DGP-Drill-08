905e991591d014e8bdb596f5ad15a659af4dde5e
Kimmingyu <d2rkjc38@gmail.com>
2025-10-16 16:02:14 +0900

    벽과 충돌시 방향전환 코드 do(self)함수에 추가

--- boy.py
+++ boy.py
@@ -37,6 +37,16 @@ class AutoRun:
 
     def do(self):
         self.boy.frame = (self.boy.frame + 1) % 8
+        if self.boy.face_dir == 1 and self.boy.dir == 1:
+            if self.boy.x <= 800:
+                self.boy.x += self.boy.dir * 5
+            else:
+                self.boy.face_dir, self.boy.dir = -1, -1
+        elif self.boy.face_dir == -1 and self.boy.dir == -1:
+            if self.boy.x >= 0:
+                self.boy.x += self.boy.dir * 5
+            else:
+                self.boy.face_dir, self.boy.dir = 1, 1
 
 
     def draw(self):
